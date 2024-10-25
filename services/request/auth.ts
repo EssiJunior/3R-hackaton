8639e10b672632faba9d55b6ff817402d355c2ea
sandjonyves <sandjonyves@gmail.com>
2024-10-25 15:56:27 +0000

    register api

---
+++ services/request/auth.ts
@@ -0,0 +1,55 @@
+import Axios from '@/Axios/Axios'
+
+
+
+interface formdData_type {
+    username: string,
+    email: string,
+    phone_number: string,
+    password: string,
+
+
+}
+
+interface params_register {
+    formData: formdData_type
+}
+
+// interface params_login{
+//    formData :formData_type
+// }
+
+
+class Auth {
+
+
+    register({ formData }: params_register) {
+
+
+        Axios.post('account/register/', formData)
+            .then((response) => {
+                console.log(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    login({ formData }: params_register) {
+
+
+        Axios.post('account/login', formData)
+            .then((response) => {
+                console.log(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+
+}
+
+
+
+export default  Auth()
